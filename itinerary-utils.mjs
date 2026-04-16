f24d87fe9a08faeb0bac74525520f780055edd2b
BrunoAlmeida87 <bruno_8709@hotmail.com>
2026-04-16 16:32:14 -0300

    Improve mobile drag behavior, fast local render, and robust date remap

---
+++ itinerary-utils.mjs
@@ -0,0 +1,38 @@
+const TAGS_VALIDAS = new Set(["fixo", "sugestao", "descanso", "viagem"]);
+
+export function escapeHtml(value) {
+  return String(value ?? "")
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
+}
+
+export function sanitizeTag(tag) {
+  return TAGS_VALIDAS.has(tag) ? tag : "sugestao";
+}
+
+export function atualizarItemEditado(itinerario, index, dadosAtualizados) {
+  return itinerario.map((item, i) => {
+    if (i !== index) return item;
+    return {
+      ...item,
+      cidade: dadosAtualizados.cidade,
+      atividade: dadosAtualizados.atividade,
+      tag: sanitizeTag(dadosAtualizados.tag)
+    };
+  });
+}
+
+export function reordenarItinerario(itinerario, orderIndices) {
+  return orderIndices.map((idx) => itinerario[idx]).filter(Boolean);
+}
+
+export function aplicarDatasFixasAoItinerario(itinerario, agendaFixa) {
+  return itinerario.map((item, index) => ({
+    ...item,
+    data: agendaFixa[index]?.data ?? item.data,
+    diaSemana: agendaFixa[index]?.diaSemana ?? item.diaSemana
+  }));
+}
